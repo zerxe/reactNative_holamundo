7cb8289d1e010b061eadc712a7c66e8d141e2e87
zerxe <amc92v@gmail.com>
2023-09-22 21:01:39 +0100

    React Native Hola Mundo

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [mensaje, setMensaje] = useState('');
+  const [contador, setContador] = useState(0);
+  const textoHolaMundo = 'Hola Mundo';
+
+  const crearHolaMundo = () => {
+    if (contador < textoHolaMundo.length) {
+      setMensaje(mensaje + textoHolaMundo.charAt(contador));
+      setContador(contador + 1);
+    } else {
+      setContador(0);
+      setMensaje('');
+    }
+  };
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Text>UT2 React Native "Hola Mundo" Adolfo Morales</Text>  
+      <Text>Para generar la palabra "Hola Mundo" pulsa "+"</Text>
       <StatusBar style="auto" />
+      <Text>{mensaje}</Text>
+      <Button title="+" onPress={crearHolaMundo} />
     </View>
   );
 }
